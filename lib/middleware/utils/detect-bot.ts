8bd25f4d7b76170dce628dd9fc610ef6ce39a512
rmthomas98 <rmthomas1998@gmail.com>
2024-11-15 10:57:50 -0500

    update detect bot

--- lib/middleware/utils/detect-bot.ts
+++ lib/middleware/utils/detect-bot.ts
@@ -3,5 +3,21 @@ import { NextRequest, userAgent } from "next/server";
 
 export const detectBot = (req: NextRequest) => {
   const ua = userAgent(req);
-  return ua.isBot;
+  if (ua) {
+    if (ua.isBot) return true;
+    /* Note:
+     * - bot is for most bots & crawlers
+     * - ChatGPT is for ChatGPT
+     * - bluesky is for Bluesky crawler
+     * - facebookexternalhit is for Facebook crawler
+     * - WhatsApp is for WhatsApp crawler
+     * - MetaInspector is for https://metatags.io/
+     * - Go-http-client/1.1 is a bot: https://user-agents.net/string/go-http-client-1-1
+     * - iframely is for https://iframely.com/docs/about (used by Notion, Linear)
+     */
+    return /bot|chatgpt|bluesky|facebookexternalhit|WhatsApp|google|baidu|bing|msn|duckduckbot|teoma|slurp|yandex|MetaInspector|Go-http-client|iframely/i.test(
+      ua.ua,
+    );
+  }
+  return false;
 };
