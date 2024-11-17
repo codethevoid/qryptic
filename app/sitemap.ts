7f152a68fdaafb26f40db6f681d6bfd97414fb5a
rmthomas98 <rmthomas1998@gmail.com>
2024-11-17 11:09:53 -0500

    update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,33 @@
 import type { MetadataRoute } from "next";
+import { readdirSync } from "fs";
+import path from "path";
 
-const sitemap = (): MetadataRoute.Sitemap => {
+const contentDir = path.join(process.cwd(), "app/main/blog/content");
+
+export const getBlogPosts = async (): Promise<MetadataRoute.Sitemap[0][]> => {
+  const categories = readdirSync(contentDir);
+  const posts = await Promise.all(
+    categories.flatMap((category) => {
+      const postFiles = readdirSync(path.join(contentDir, category));
+      return postFiles.map(async (postFile) => {
+        const { metadata } = await import(`@/app/main/blog/content/${category}/${postFile}`);
+        return {
+          url: `https://qryptic.io/blog/${category}/${postFile.replace(".mdx", "")}`,
+          lastModified: new Date(metadata.updatedAt ? metadata.updatedAt : metadata.date),
+          changeFrequency: "monthly" as const,
+          priority: 0.6,
+        };
+      });
+    }),
+  );
+
+  return posts;
+};
+
+const sitemap = async (): Promise<MetadataRoute.Sitemap> => {
+  // get blog posts
+  const posts: MetadataRoute.Sitemap[0][] = await getBlogPosts();
+
   return [
     {
       url: "https://qryptic.io",
@@ -18,20 +45,27 @@ const sitemap = (): MetadataRoute.Sitemap => {
       url: "https://qryptic.io/contact",
       lastModified: new Date(),
       changeFrequency: "yearly",
-      priority: 0.5,
+      priority: 0.6,
     },
     {
       url: "https://qryptic.io/legal/privacy",
       lastModified: new Date(),
       changeFrequency: "yearly",
-      priority: 0.5,
+      priority: 0.3,
     },
     {
       url: "https://qryptic.io/legal/terms",
       lastModified: new Date(),
       changeFrequency: "yearly",
-      priority: 0.5,
+      priority: 0.3,
     },
+    {
+      url: "https://qryptic.io/blog",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
+    },
+    ...posts,
   ];
 };
 
